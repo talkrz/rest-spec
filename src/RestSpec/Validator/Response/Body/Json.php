bf36728f710c0164dd26a02a46f38d39922c6778
talkrz <krzysztof.talajko@gmail.com>
2015-05-11 15:05:55 +0200

    Extract method

--- src/RestSpec/Validator/Response/Body/Json.php
+++ src/RestSpec/Validator/Response/Body/Json.php
@@ -32,25 +32,7 @@ class Json extends Validator
 
         // then if exact body specified check it matches spec
         if ($responseSpec->getBody()) {
-            $actualBody = (string) $response->getBody();
-
-            $actualBodyData = json_decode($actualBody, true);
-
-            if ($actualBodyData === $responseSpec->getBody()) {
-                $output->writeln(
-                    sprintf(
-                        "\t\tResponse body is valid JSON:\n<info>%s</info>",
-                        $this->getOutput()->formatArray($actualBodyData, 3)
-                    )
-                );
-            } else {
-                $message = sprintf("\t\t<error>JSON in body of the response is invalid, actual:</error>\n%s\n\t\t<error>Expected:</error>\n%s",
-                    $this->getOutput()->formatArray($actualBodyData, 3),
-                    $this->getOutput()->formatArray($responseSpec->getBody(), 3)
-                );
-                $output->writeln($message);
-                $this->addViolation($message);
-            }
+            $this->validateBody($response, $responseSpec);
         } elseif($constraint = $responseSpec->getBodyConstraint()) {
             $actualBody = (string) $response->getBody();
 
@@ -87,5 +69,30 @@ class Json extends Validator
         }
 
         return $this->isValid();
+    }
+
+    public function validateBody(Response $response, Spec\Response $responseSpec)
+    {
+        $output = $this->getOutput()->getOutput();
+
+        $actualBody = (string) $response->getBody();
+
+        $actualBodyData = json_decode($actualBody, true);
+
+        if ($actualBodyData === $responseSpec->getBody()) {
+            $output->writeln(
+                sprintf(
+                    "\t\tResponse body is valid JSON:\n<info>%s</info>",
+                    $this->getOutput()->formatArray($actualBodyData, 3)
+                )
+            );
+        } else {
+            $message = sprintf("\t\t<error>JSON in body of the response is invalid, actual:</error>\n%s\n\t\t<error>Expected:</error>\n%s",
+                $this->getOutput()->formatArray($actualBodyData, 3),
+                $this->getOutput()->formatArray($responseSpec->getBody(), 3)
+            );
+            $output->writeln($message);
+            $this->addViolation($message);
+        }
     }
 }
