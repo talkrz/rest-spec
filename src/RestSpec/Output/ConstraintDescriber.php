6ec9c79fd022d79ab0837104be7fa135fa661411
talkrz <krzysztof.talajko@gmail.com>
2015-05-11 14:28:10 +0200

    Extract functions for collection and all constraints

--- src/RestSpec/Output/ConstraintDescriber.php
+++ src/RestSpec/Output/ConstraintDescriber.php
@@ -29,33 +29,58 @@ class ConstraintDescriber
         $className = 'RestSpec\\Output\\ConstraintDescriber\\' . $className;
 
         if ($constraint instanceof All) {
-            $output .= 'Each of results ';
-            foreach ($constraint->constraints as $nestedConstraint) {
-                $output .= $this->describe($nestedConstraint);
-            }
-        } elseif ($constraint instanceof Collection) {
-            $output .= 'should be an array with following properties:' . PHP_EOL;
-
-            $nestedOutput = '';
-            foreach ($constraint->fields as $fieldName => $fieldConstrain) {
-                $nestedOutput .= "<comment>" . $fieldName . "</comment> ";
-
-                if ($fieldConstrain->constraints) {
-                    foreach ($fieldConstrain->constraints as $nestedConstraint) {
-                        $nestedOutput .= $this->describe($nestedConstraint) . PHP_EOL;
-                    }
-                } else {
-                    $nestedOutput .= '[no constraints]' . PHP_EOL;
-                }
-            }
-
-            $output .= indentValue($nestedOutput, 1);
-
+            $output .= $this->describeAll($constraint);
+        } else if ($constraint instanceof Collection) {
+            $output .= $this->describeCollection($constraint);
         } else if (class_exists($className)) {
             $describer = new $className;
             $output .= $describer->describe($constraint);
         } else {
             $output .= get_class($constraint);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Describe collection constraint
+     *
+     * @param Collection $constraint
+     * @return string
+     */
+    private function describeCollection(Collection $constraint)
+    {
+        $output = 'should be an array with following properties:' . PHP_EOL;
+
+        $nestedOutput = '';
+        foreach ($constraint->fields as $fieldName => $fieldConstrain) {
+            $nestedOutput .= "<comment>" . $fieldName . "</comment> ";
+
+            if ($fieldConstrain->constraints) {
+                foreach ($fieldConstrain->constraints as $nestedConstraint) {
+                    $nestedOutput .= $this->describe($nestedConstraint) . PHP_EOL;
+                }
+            } else {
+                $nestedOutput .= '[no constraints]' . PHP_EOL;
+            }
+        }
+
+        $output .= indentValue($nestedOutput, 1);
+
+        return $output;
+    }
+
+    /**
+     * Describe "all" constraint
+     *
+     * @param All $constraint
+     * @return string
+     */
+    private function describeAll(All $constraint)
+    {
+        $output = 'Each of results ';
+        foreach ($constraint->constraints as $nestedConstraint) {
+            $output .= $this->describe($nestedConstraint);
         }
 
         return $output;
